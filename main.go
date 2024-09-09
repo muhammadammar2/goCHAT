40b4784c0247e6bb14b897ca91b7be2278c83d26
muhammadammar2 <uExistentialist@proton.me>
2024-09-09 23:52:46 +0500

    GORM

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"dummy/models"
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Database connection setup
+	dsn := "root:123@tcp(127.0.0.1:3306)/goo?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	err = db.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
