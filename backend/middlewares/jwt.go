f4df1c99df41256bc401d8d6ac0672acd991791f
muhammadammar2 <uExistentialist@proton.me>
2024-09-27 22:11:34 +0500

    solved the bearer xD

--- backend/middlewares/jwt.go
+++ backend/middlewares/jwt.go
@@ -2,6 +2,7 @@ package middlewares
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/muhammadammar2/goCHAT/utils"
@@ -14,13 +15,14 @@ func JWTMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Missing token"})
 		}
 
-		claims , err := utils.VerifyJWT(token)
+		token = strings.TrimPrefix(token, "Bearer ") 
+
+		claims, err := utils.VerifyJWT(token)
 		if err != nil {
 			return c.JSON(http.StatusUnauthorized, map[string]string{"error": "Invalid token"})
 		}
 
 		c.Set("user_id", claims.UserID)
-
 		return next(c)
 	}
 }
