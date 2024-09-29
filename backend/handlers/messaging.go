d8d7daeac9281fc1a881a482791dd1bb53d71e83
muhammadammar2 <uExistentialist@proton.me>
2024-09-29 20:22:16 +0500

    messages are being sent and recieved fine in the postman

--- backend/handlers/messaging.go
+++ backend/handlers/messaging.go
@@ -1,33 +1,40 @@
 package handlers
 
 import (
-	"net/http"
+	"fmt"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
 )
 
-var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool {
-        return true // Allow all origins for testing; adjust as necessary for production
-    },
-}
+var (
+	upgrader = websocket.Upgrader{}
+	clients  = make(map[*websocket.Conn]bool) 
+)
 
 func WebSocketHandler(c echo.Context) error {
-    ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-    if err != nil {
-        return err
-    }
-    defer ws.Close()
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
 
-    for {
-        _, msg, err := ws.ReadMessage()
-        if err != nil {
-            return err
-        }
-        // Echo the message back to the client (for testing purposes)
-        if err := ws.WriteMessage(websocket.TextMessage, msg); err != nil {
-            return err
-        }
-    }
+	clients[ws] = true
+	defer delete(clients, ws)
+
+	for {
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			break
+		}
+		fmt.Printf("Received message: %s\n", msg)
+
+		for client := range clients {
+			if err := client.WriteMessage(websocket.TextMessage, msg); err != nil {
+				delete(clients, client)
+			}
+		}
+	}
+
+	return nil
 }
