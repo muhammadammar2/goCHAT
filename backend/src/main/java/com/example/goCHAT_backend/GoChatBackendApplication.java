661d7694479e977341983cd492adc5fc96ee242a
muhammadammar2 <uExistentialist@proton.me>
2024-10-04 15:30:56 +0500

    let's get started 1 more time

---
+++ backend/src/main/java/com/example/goCHAT_backend/GoChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.goCHAT_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoChatBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/goCHAT_backend/GoChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goCHAT_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
